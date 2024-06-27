fb31b4308044cc92047eaf1aaf9eea6ce4c78f07
shubhampal <shubhampalatwork@gmail.com>
2024-06-27 21:15:46 +0530

    updated UI for theme dropdown

--- src/App.js
+++ src/App.js
@@ -2,9 +2,18 @@ import React, { useState } from 'react';
 import styled, { ThemeProvider } from 'styled-components';
 import CodeEditor from './CodeEditor';
 import { lightTheme, darkTheme, systemTheme } from './themes';
+import './App.css';
+
+const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  margin-bottom: 16px;
+  padding: 1rem;
+`;
 
 const ThemeSelector = styled.select`
-  margin-bottom: 16px;
   padding: 8px;
   border-radius: 4px;
   border: 1px solid ${props => props.theme.borderColor};
@@ -30,12 +39,14 @@ const App = () => {
   return (
     <ThemeProvider theme={getCurrentTheme()}>
       <div className="App">
-        <h1>Simple Code Editor</h1>
-        <ThemeSelector onChange={handleThemeChange} value={theme}>
-          <option value="light">Light</option>
-          <option value="dark">Dark</option>
-          <option value="system">System</option>
-        </ThemeSelector>
+        <Header>
+          <h1>Simple Code Editor</h1>
+          <ThemeSelector onChange={handleThemeChange} value={theme}>
+            <option value="light">Light</option>
+            <option value="dark">Dark</option>
+            <option value="system">System</option>
+          </ThemeSelector>
+        </Header>
         <CodeEditor theme={getCurrentTheme()} />
       </div>
     </ThemeProvider>
